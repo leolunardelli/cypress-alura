6674202ea7ee0f1a2acad7410bfa2389b3732496
https;//github.com/leolunardelli <leo.luna94@gmail.com>
2022-11-05 13:37:27 -0300

    commit ignore

---
+++ cypress/e2e/3-alurapic/alurapic.spec.cy.js
@@ -0,0 +1,45 @@
+describe('Login e registro de usuarios alura pic', () => {
+
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com')
+    })
+
+    it('verifica mensagens validacao', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+        cy.contains('ng-component', 'Register to embrace a new world!').should('be.visible');
+    })
+
+    it('verifica mensagens de email invalido', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('leonardo');
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+
+    })
+
+    it('verifica mensagem de necessita no minimo dois caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="fullName"]').type('l');
+        cy.get('input[formcontrolname="userName"]').type('l');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+    })
+
+    it('verifica mensagem de senha menor que oito digitos', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+
+    })
+
+
+})
